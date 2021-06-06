65d4150f87e1d683f6e8a1866ba18bd35e4af3da
Umut Guder <mennanguder@gmail.com>
2021-06-06 15:40:49 +0300

    Giving Error message in modal pop up

--- src/C/components/NewExpense/ExpenseForm.js
+++ src/C/components/NewExpense/ExpenseForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 import styles from "./ExpenseForm.module.css";
 
 const ExpenseForm = (props) => {
@@ -10,6 +11,8 @@ const ExpenseForm = (props) => {
   const [isValidTitle, setIsValidTitle] = useState(true);
   const [isValidAmount, setIsValidAmount] = useState(true);
   const [isValidDate, setIsValidDate] = useState(true);
+
+  const [error, setError] = useState();
 
   const titleChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
@@ -37,10 +40,18 @@ const ExpenseForm = (props) => {
 
     if (enteredTitle.trim().length === 0) {
       setIsValidTitle(false);
+      setError({
+        title: "Invalid Input",
+        message: "Please enter a valid Title",
+      });
     }
 
     if (enteredAmount.trim().length === 0) {
       setIsValidAmount(false);
+      setError({
+        title: "Invalid Input",
+        message: "Please enter a valid amount",
+      });
     }
 
     if (enteredDate.trim().length === 0) {
@@ -68,57 +79,70 @@ const ExpenseForm = (props) => {
     setEnteredDate("");
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <form onSubmit={submitHandler}>
-      <div className={styles["new-expense__controls"]}>
-        <div
-          className={`${styles["new-expense__control"]} ${
-            !isValidTitle && styles.invalid
-          }`}
-        >
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        ></ErrorModal>
+      )}
+      <form onSubmit={submitHandler}>
+        <div className={styles["new-expense__controls"]}>
+          <div
+            className={`${styles["new-expense__control"]} ${
+              !isValidTitle && styles.invalid
+            }`}
+          >
+            <label>Title</label>
+            <input
+              type="text"
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+            />
+          </div>
+          <div
+            className={`${styles["new-expense__control"]} ${
+              !isValidAmount && styles.invalid
+            }`}
+          >
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={enteredAmount}
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div
+            className={`${styles["new-expense__control"]} ${
+              !isValidDate && styles.invalid
+            }`}
+          >
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              value={enteredDate} // Two way binding
+              onChange={dateChangeHandler}
+            />
+          </div>
         </div>
-        <div
-          className={`${styles["new-expense__control"]} ${
-            !isValidAmount && styles.invalid
-          }`}
-        >
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
+        <div className={styles["new-expense__actions"]}>
+          <Button type="button" onClick={props.onCancel}>
+            Cancel
+          </Button>
+          <Button type="submit">Add Expense</Button>
         </div>
-        <div
-          className={`${styles["new-expense__control"]} ${
-            !isValidDate && styles.invalid
-          }`}
-        >
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={enteredDate} // Two way binding
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className={styles["new-expense__actions"]}>
-        <Button type="button" onClick={props.onCancel}>
-          Cancel
-        </Button>
-        <Button type="submit">Add Expense</Button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
