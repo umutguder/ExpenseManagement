5e6f03dcc4de800381718af92ce0075aa0476a7c
Umut Guder <mennanguder@gmail.com>
2021-06-07 10:22:44 +0300

    Ref usage example

--- src/C/components/NewExpense/ExpenseForm.js
+++ src/C/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../UI/Button";
 import Wrapper from "../Helpers/Wrapper";
 import ErrorModal from "../UI/ErrorModal";
 import styles from "./ExpenseForm.module.css";
 
 const ExpenseForm = (props) => {
+  const titleInputRef = useRef();
+  /* ref keeps real dom element*/
+  const amountInputRef = useRef();
+  const dateInputRef = useRef();
+
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -39,7 +44,17 @@ const ExpenseForm = (props) => {
   const submitHandler = (event) => {
     event.preventDefault();
 
-    if (enteredTitle.trim().length === 0) {
+    /* Refs are better to use if only read is required 
+    read/write => use hooks with useState
+    Setting value for refs are not react way 
+    it uses html dom manipulation thus not recommended
+    Controlled component: the internal state is controlled by react 
+    if u add ref to an input and set its value then it is controlled*/
+    const enteredTitleRef = titleInputRef.current.value;
+    const enteredAmountRef = amountInputRef.current.value;
+    const enteredDateRef = dateInputRef.current.value;
+
+    if (enteredTitleRef.trim().length === 0) {
       setIsValidTitle(false);
       setError({
         title: "Invalid Input",
@@ -47,7 +62,7 @@ const ExpenseForm = (props) => {
       });
     }
 
-    if (enteredAmount.trim().length === 0) {
+    if (enteredAmountRef.trim().length === 0) {
       setIsValidAmount(false);
       setError({
         title: "Invalid Input",
@@ -55,22 +70,22 @@ const ExpenseForm = (props) => {
       });
     }
 
-    if (enteredDate.trim().length === 0) {
+    if (enteredDateRef.trim().length === 0) {
       setIsValidDate(false);
     }
 
     if (
-      enteredTitle.trim().length === 0 ||
-      enteredAmount.trim().length === 0 ||
-      enteredDate.trim().length === 0
+      enteredTitleRef.trim().length === 0 ||
+      enteredAmountRef.trim().length === 0 ||
+      enteredDateRef.trim().length === 0
     ) {
       return;
     }
 
     const expenseData = {
-      title: enteredTitle,
-      amount: +enteredAmount,
-      date: new Date(enteredDate),
+      title: enteredTitleRef,
+      amount: +enteredAmountRef,
+      date: new Date(enteredDateRef),
     };
 
     props.onSaveExpenseData(expenseData);
@@ -105,6 +120,7 @@ const ExpenseForm = (props) => {
               type="text"
               value={enteredTitle}
               onChange={titleChangeHandler}
+              ref={titleInputRef}
             />
           </div>
           <div
@@ -119,6 +135,7 @@ const ExpenseForm = (props) => {
               step="0.01"
               value={enteredAmount}
               onChange={amountChangeHandler}
+              ref={amountInputRef}
             />
           </div>
           <div
@@ -133,6 +150,7 @@ const ExpenseForm = (props) => {
               max="2022-12-31"
               value={enteredDate} // Two way binding
               onChange={dateChangeHandler}
+              ref={dateInputRef}
             />
           </div>
         </div>
