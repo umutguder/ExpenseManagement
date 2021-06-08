d68e859f69349a46c701bc983e5e85a5fce49af6
Umut Guder <mennanguder@gmail.com>
2021-06-08 16:50:00 +0300

    useEffect hook usage with timer

--- src/C/components/UI/Button.js
+++ src/C/components/UI/Button.js
@@ -4,9 +4,10 @@ import classes from "./Button.module.css";
 function Button(props) {
   return (
     <button
-      className={classes.button}
+      className={`${classes.button} ${props.className}`}
       type={props.type || "button"}
       onClick={props.onClick}
+      disabled={props.disabled}
     >
       {props.children}
     </button>
